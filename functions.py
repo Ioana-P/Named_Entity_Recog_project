3f519efc569b14544234b4ae59419b6b0b768e03
Ioana Preoteasa <ioanacpreoteasa@gmail.com>
2020-07-31 17:58:58 +0100

    some interp of eli5 weights; more tuning; pickled model

--- functions.py
+++ functions.py
@@ -72,6 +72,27 @@ stop_words += list(string.punctuation)
 
 #################################EDA#####################################
 
+def example_output(num, data, estimator, target_data, word_arg = 'word.lower()'):
+    '''Short function that produces a quick inspection table so we can compare how 
+    our CRF model performs against the labelled sentences. 
+    Params:
+    num - (int) index of sentence in the data
+    data - (list) input data that the prediction will be made on. Must be of form of list
+    of lists of dicts
+    estimator - (object) model to use for prediction - has to be sklearn crf
+    target_data - (list) data from which ground truth labels are to be retrieved; has
+    to be list of list of strings
+    word_arg - (str) which dictionary argument to access to retrieve the original, 
+    lowercased word - this will depend on how the initial data was defined. 
+    
+    Returns:
+    pandas Dataframe with 3 columns: True label; predicted label and the Word
+    '''
+    predicted_table = pd.DataFrame(columns=['True', 'Pred', 'Word'])
+    predicted_table['True'] = target_data[num]
+    predicted_table['Word'] = [word[word_arg] for word in data[num]]
+    predicted_table['Pred'] = estimator.predict([data[num]])[0]
+    return predicted_table
 
 
 #################################SUMMARY TABLES CREATION#####################################
